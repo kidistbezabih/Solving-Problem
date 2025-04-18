68a413529b9691f136deac9918a0a8599be4f724
kidistbezabih <97032250+kidistbezabih@users.noreply.github.com>
2025-04-18 19:45:51 +0300

    Time: 2 ms (95.33%) | Memory: 19.2 MB (58.84%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -2,9 +2,10 @@ class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         ans = []
         res = [0]
+        n = len(graph)
 
         def backtrack(i):
-            if i == len(graph) - 1:
+            if i == n - 1:
                 ans.append(res[:])
                 return
 
