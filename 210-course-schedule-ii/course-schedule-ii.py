d752f4113284366b47fb99113d5a554c78b87f6e
kidistbezabih <97032250+kidistbezabih@users.noreply.github.com>
2025-05-25 16:33:08 +0300

    Time: 2 ms (82.10%) | Memory: 18.9 MB (84.95%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,27 +1,24 @@
 class Solution:
-    def topSort(self, node, graph, color, order):
-        if color[node] == 1:
-            return False
-        if color[node] == 2:
-            return True
-        color[node] = 1
-        for neig in graph[node]:
-            if not self.topSort(neig, graph, color, order):
-                return False
-        color[node] = 2
-        order.append(node)
-        return True
-
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph = [[] for _ in range(numCourses)]
-        color = [0 for _ in range(numCourses)]
+        graph = [[] for i in range(numCourses)]
         order = []
+        indegree = [0] * numCourses
+        queue = deque()
 
         for course, pre in prerequisites:
             graph[pre].append(course)
-
-        for node in range(numCourses):
-            if color[node] == 0:
-                if not self.topSort(node, graph, color, order):
-                    return []
-        return order[::-1]
+            indegree[course] += 1
+        
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                queue.append(i)
+        
+        while queue:
+            pre = queue.popleft()
+            order.append(pre)
+            for course in graph[pre]:
+                indegree[course] -= 1
+                if indegree[course] == 0:
+                    queue.append(course)
+        return order if len(order) == numCourses else []
+            
