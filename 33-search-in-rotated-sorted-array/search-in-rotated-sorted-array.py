bd25fdc6e2cc2b54f314525cba9efe16637f1686
kidistbezabih <97032250+kidistbezabih@users.noreply.github.com>
2025-06-01 17:12:55 +0300

    Time: 0 ms (100.00%) | Memory: 18.1 MB (66.22%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -0,0 +1,37 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def findIndex(left, right):
+            while left <= right:
+                mid = left + (right - left)//2
+
+                if nums[mid] > nums[-1]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return left
+
+        n = len(nums)
+        left, right = 0, n - 1
+        pivot = findIndex(left, right)
+        print(pivot)
+        
+        if nums[pivot] <= target <= nums[-1]:
+            left, right = pivot, n-1
+        else:
+            left, right = 0, pivot
+
+        print(right, left)
+
+        while left <= right:
+            mid = left + (right - left)//2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
+
+        
