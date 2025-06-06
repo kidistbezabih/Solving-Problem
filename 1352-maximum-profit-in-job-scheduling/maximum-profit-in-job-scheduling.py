fa87e092388ee472b70e41719d60793682665312
kidistbezabih <97032250+kidistbezabih@users.noreply.github.com>
2025-06-06 14:36:32 +0300

    Time: 171 ms (63.67%) | Memory: 68.1 MB (10.81%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
@@ -1,33 +1,18 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-
         n = len(startTime)
-        def sort_jobs():
-            start_end_pair = sorted((startTime[i], endTime[i], profit[i]) for i in range(n))
-            for i in range(n):
-                startTime[i], endTime[i], profit[i] = start_end_pair[i]
-        
-        sort_jobs()
 
-        hash_map = defaultdict(int)
-        curr_max = 0
-        for i in range(n - 1, -1, -1):
-            next_indx = bisect_left(startTime, endTime[i])
-            include = profit[i] + hash_map[next_indx]
-            curr_max = max(include, curr_max)
-            hash_map[i] = curr_max
-        
-        return curr_max
+        job_schedule = sorted([(startTime[i], endTime[i], profit[i]) for i in range(n)])
+        startTime = [i[0] for i in job_schedule ]
+        memo = {}
 
-        @cache
-        def dp(indx):
-            if indx == n:
-                return 0
-            
-            next_indx = bisect_left(startTime, endTime[indx])
-            include = profit[indx] + dp(next_indx)
-            exclude = dp(indx + 1)
-            return max(include, exclude)
-        
+        def dp(i):
+            if i in memo:
+                return memo[i]
+            if i == n:return 0
+            l,r = i+1, n-1
+            next_index = bisect_left(startTime, job_schedule[i][1])
+
+            memo[i] =  max( job_schedule[i][2]+ dp(next_index), dp(i+1))
+            return memo[i]
         return dp(0)
-        
