dbe32a59f124322c7186d86c2d11d00830234794
kidistbezabih <97032250+kidistbezabih@users.noreply.github.com>
2025-06-11 10:38:05 +0300

    Time: 119 ms (72.03%) | Memory: 58.1 MB (18.58%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,31 +1,32 @@
 class RandomizedSet:
 
     def __init__(self):
+        self.randomSet = []
         self.dct = {}
-        self.random = []
-        self.index = 0  
+        self._index = 0
 
     def insert(self, val: int) -> bool:
         if val in self.dct:
             return False
-        self.random.append(val)
-        self.dct[val] = self.index
-        self.index += 1
+        self.dct[val] = self._index
+        self.randomSet.append(val)
+        self._index += 1
         return True
-        
+
     def remove(self, val: int) -> bool:
         if val not in self.dct:
             return False
-        ind = self.dct[val]
-        self.random[self.index - 1], self.random[ind] = self.random[ind], self.random[self.index - 1]
-        self.dct[self.random[ind]] = ind
-        del self.dct[val]
-        self.random.pop()
-        self.index -= 1
+        idx = self.dct[val]
+        print(idx)
+        self.randomSet[idx], self.randomSet[-1] = self.randomSet[-1], self.randomSet[idx] 
+        self.dct[self.randomSet[idx]] = idx
+        removed = self.randomSet.pop()
+        self.dct.pop(removed)
+        self._index -= 1
         return True
 
     def getRandom(self) -> int:
-        return random.choice(self.random)
+        return random.choice(self.randomSet)
         
 
 
