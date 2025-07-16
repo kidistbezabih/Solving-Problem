a0a8145a40a361d0eff80365dfaeb950dea957c8
kidistbezabih <97032250+kidistbezabih@users.noreply.github.com>
2025-07-16 11:03:03 +0300

    Time: 150 ms (44.42%) | Memory: 78 MB (91.66%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,53 +1,81 @@
-class Node:
-    def __init__(self, key=None, value=None):
-        self.value = value
+class Linkedlist:
+    def __init__(self, key, val):
+        self.value = val
         self.key = key
-        self.next = None 
         self.prev = None
+        self.next = None
 
 class LRUCache:
+
     def __init__(self, capacity: int):
-        self.cache = {}
+        self.memo = {}
         self.capacity = capacity
 
-        self.lru = Node()
-        self.mru = Node()
-        self.lru.next = self.mru
-        self.mru.prev = self.lru
+        self.head = Linkedlist(0, 0) 
+        self.tail = Linkedlist(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
 
     def get(self, key: int) -> int:
-        if key in self.cache:
-            node = self.cache[key]
-            self.remove(node)
-            self.add(node)
-            return node.value
-        return -1
-            
-    def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.remove(self.cache[key])
+        if not key in self.memo:
+            return -1
         
-        node = Node(key, value)
-        self.cache[key] = node
+        self.remove(self.memo[key])
+        self.insert_last(self.memo[key])
 
-        if len(self.cache) > self.capacity:
-            l = self.lru.next
-            self.remove(l)
-            del self.cache[l.key]
+        return self.memo[key].value     
 
-        self.add(node)
-        self.cache[key] = node
+    def put(self, key: int, value: int) -> None:
+        if key in self.memo:
+            self.memo[key].value = value
+            self.remove(self.memo[key])
 
-    def add(self, node:Node) -> None:
-        prev = self.mru.prev
-        prev.next = node
-        node.prev = prev
-        node.next = self.mru
-        self.mru.prev = node
+        else:
+            if self.capacity == 0:
+                node_to_delete = self.head.next.key
+                self.remove(self.memo[node_to_delete])
+                del self.memo[node_to_delete]
+                
+            else:
+                self.capacity -= 1
+            self.memo[key] = Linkedlist(key, value)
+        self.insert_last(self.memo[key])
 
-    def remove(self, node: Node) -> None:
+    def remove(self, node):
         node.prev.next = node.next
         node.next.prev = node.prev
+
+    
+    def insert_last(self, node):
+        last_node = self.tail.prev  
+        last_node.next = node
+        node.prev = last_node
+        node.next = self.tail
+        self.tail.prev = node
+
+
+    
+
+
+            
+
+'''
+capacity = 2
+dic = {
+    1:1,
+    2:2,
+    3:3,
+}
+order = {1:1,
+2:2,
+3:3,
+}
+
+queue = [1,2,3]
+
+'''     
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
