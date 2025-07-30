7af10140863aa55c8de3a40aeb18148efbc9965f
kidistbezabih <97032250+kidistbezabih@users.noreply.github.com>
2025-07-30 20:19:50 +0300

    Time: 1660 ms (28.98%) | Memory: 18 MB (61.70%) - LeetSync

--- 3949-minimum-number-of-primes-to-sum-to-target/minimum-number-of-primes-to-sum-to-target.py
+++ 3949-minimum-number-of-primes-to-sum-to-target/minimum-number-of-primes-to-sum-to-target.py
@@ -1,29 +1,33 @@
 class Solution:
-    def generate_primes(self, m):
-        primes = []
-        i = 2
-        while len(primes) < m:
-            if self.is_prime(i):
-                primes.append(i)
-            i+=1
-        return primes
-
     def is_prime(self, num):
         if num < 2:
             return False
+
         for i in range(2, int(sqrt(num))+1):
-            if  num % i == 0:
+            if num % i == 0:
                 return False
         return True
 
+    def mprimes(self, m):
+        primes = []
+        n = 0
+        num = 2
+
+        while n < m:
+            if self.is_prime(num):
+                primes.append(num)
+                n += 1
+            num += 1
+        return primes
+         
     def minNumberOfPrimes(self, n: int, m: int) -> int:
-        mprimes = self.generate_primes(m)
-        dp = [float('inf')]*(n+1)
+        m_primes = self.mprimes(m)
+        dp = [float("inf")] * (n+1)
         dp[0] = 0
 
-        for prime in mprimes:
-            for i in range(prime, n+1):
-                dp[i] = min(dp[i], dp[i-prime] + 1)
 
-        return dp[n] if dp[n] != math.inf else -1
-
+        for i in range(1, n+1):
+            for prime in m_primes:
+                if i - prime >= 0:
+                    dp[i] = min(dp[i], dp[i-prime]+1)
+        return dp[n] if dp[n] != float("inf") else -1
