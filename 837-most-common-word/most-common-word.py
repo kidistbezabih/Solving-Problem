44ecbf43726eb8cd7a0057bbc0bc15a2f22521b8
kidistbezabih <97032250+kidistbezabih@users.noreply.github.com>
2025-08-05 15:44:33 +0300

    Time: 1 ms (74.98%) | Memory: 17.8 MB (74.59%) - LeetSync

--- 837-most-common-word/most-common-word.py
+++ 837-most-common-word/most-common-word.py
@@ -1,26 +1,22 @@
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        ans = ""
-        max_count = 0
-        count_word = defaultdict(int)
+        word = []
+        wordList = defaultdict(int)
         banned = set(banned)
-        buffer = []
-        n = len(paragraph)
+        mx_freq = 0
+        ans = ""
 
-        for i, c in enumerate(paragraph):
-            if c.isalnum():
-                print(c)
-                buffer.append(c.lower())
-                if i != n-1:
-                    continue
-        
-            if buffer:
-                word = "".join(buffer)
-                if word not in banned:
-                    count_word[word] += 1
-                    if max_count < count_word[word]:
-                        ans = word
-                        max_count = count_word[word]
-            buffer = []
+        for i, char in enumerate(paragraph):
+            if char.isalpha():
+                word.append(char.lower())
+            if char == " " or  i == len(paragraph)-1 or char == ',':
+                w = ''.join(word)
+                if w and w not in banned:
+                    wordList[w] += 1
+                    if wordList[w] > mx_freq:
+                        mx_freq = wordList[w]
+                        ans = w
+                word = []
+
         return ans
-
+            
